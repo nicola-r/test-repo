59a2ad4a71c0b893150a408ada82c553dfa3427f
Nicola <cottonfiok@gmail.com>
2018-12-13 11:42:20 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -21,7 +21,9 @@ get_header(); ?>
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyseventeen' ); ?></h1>
 				</header><!-- .page-header -->
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyseventeen' ); ?></p>
+					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyseventeen' ); 
+						echo "test";
+					?></p>
 
 					<?php get_search_form(); ?>
 
